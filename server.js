998384d4910611ce12899860a21ff185bc751e46
seremwen <seremwe.81@gmail.com>
2022-07-29 03:23:15 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/dashboard-seremwe'));
+app.use(express.static(__dirname + '/dist/dashboard_seremwe'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/dashboard-seremwe/index.html'));});
+    '/dist/dashboard_seremwe/index.html'));});
 app.listen(process.env.PORT || 8080);
